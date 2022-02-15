fbebb788ab8c3edbe29bce97ade72967c0506b6c
liorsbg <sabag.lior@gmail.com>
2022-02-15 12:50:28 +0000

    restored to original

--- src/components/keyboard/Keyboard.tsx
+++ src/components/keyboard/Keyboard.tsx
@@ -4,52 +4,50 @@ import { useEffect } from 'react'
 import { ENTER_TEXT, DELETE_TEXT } from '../../constants/strings'
 
 type Props = {
-  // onChar: (value: string) => void
-  // onDelete: () => void
-  // onEnter: () => void
-  onClick: (value: string) => void
+  onChar: (value: string) => void
+  onDelete: () => void
+  onEnter: () => void
   guesses: string[]
   isRevealing?: boolean
 }
 
 export const Keyboard = ({
-  // onChar,
-  // onDelete,
-  // onEnter,
-  onClick,
+  onChar,
+  onDelete,
+  onEnter,
   guesses,
   isRevealing,
 }: Props) => {
   const charStatuses = getStatuses(guesses)
 
-  // const onClick = (value: string) => {
-  //   if (value === 'ENTER') {
-  //     onEnter()
-  //   } else if (value === 'DELETE') {
-  //     onDelete()
-  //   } else {
-  //     onChar(value)
-  //   }
-  // }
+  const onClick = (value: string) => {
+    if (value === 'ENTER') {
+      onEnter()
+    } else if (value === 'DELETE') {
+      onDelete()
+    } else {
+      onChar(value)
+    }
+  }
 
-  // useEffect(() => {
-  //   const listener = (e: KeyboardEvent) => {
-  //     if (e.code === 'Enter') {
-  //       onClick('ENTER')
-  //     } else if (e.code === 'Backspace') {
-  //       onClick('DELETE')
-  //     } else {
-  //       const key = e.key.toUpperCase()
-  //       if (key.length === 1 && key >= 'A' && key <= 'Z') {
-  //         onClick(key)
-  //       }
-  //     }
-  //   }
-  //   window.addEventListener('keyup', listener)
-  //   return () => {
-  //     window.removeEventListener('keyup', listener)
-  //   }
-  // }, [onClick])
+  useEffect(() => {
+    const listener = (e: KeyboardEvent) => {
+      if (e.code === 'Enter') {
+        onEnter()
+      } else if (e.code === 'Backspace') {
+        onDelete()
+      } else {
+        const key = e.key.toUpperCase()
+        if (key.length === 1 && key >= 'A' && key <= 'Z') {
+          onChar(key)
+        }
+      }
+    }
+    window.addEventListener('keyup', listener)
+    return () => {
+      window.removeEventListener('keyup', listener)
+    }
+  }, [onEnter, onDelete, onChar])
 
   return (
     <div>
